92c06f1e8c2d81cb771ad2085ff8e594ebae4196
FIKRI <fikriit22@gmail.com>
2026-06-27 22:16:48 +0700

    A simple Hangman Game about Indonesian basic knowledge. (Game Hangman yang bisa menambah pengetahuanmu tentang Indonesia) Resources that we used for this game: https://www.youtube.com/watch?v=hSSdc8vKP1I&t=1542s

---
+++ scripts/word-list.js
@@ -0,0 +1,34 @@
+const wordList = [
+    { 
+        word: "merdeka", 
+        hint: "Status Indonesia sejak tanggal 17 Agustus 1945" 
+    },
+    { 
+        word: "soekarno", 
+        hint: "Presiden pertama sekaligus proklamator Republik Indonesia" 
+    },
+    { 
+        word: "pancasila", 
+        hint: "Lima dasar atau ideologi negara Indonesia" },
+    { 
+        word: "belanda", 
+        hint: "Negara Eropa yang menjajah Indonesia selama ratusan tahun" },
+    { 
+        word: "jepang", 
+        hint: "Negara Asia yang menjajah Indonesia sebelum kemerdekaan" },
+    { 
+        word: "proklamasi", 
+        hint: "Teks pernyataan kemerdekaan yang dibacakan pada 17 Agustus" },
+    { 
+        word: "pemuda", 
+        hint: "Sumpah ... , ikrar persatuan yang lahir pada 28 Oktober 1928" },
+    { 
+        word: "rupiah", 
+        hint: "Mata uang resmi yang digunakan oleh negara Indonesia" },
+    { 
+        word: "jakarta", 
+        hint: "Kota tempat pembacaan teks proklamasi kemerdekaan" },
+    { 
+        word: "bendera", 
+        hint: "Sang Saka Merah Putih adalah sebutan untuk ... Indonesia" }
+]
